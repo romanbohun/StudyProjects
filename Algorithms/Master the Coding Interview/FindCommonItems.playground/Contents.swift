265e4c49238c788234d6c9934989aaa896e549cf
Roman Bohun <roman.v.bogun@hotmail.com>
2020-02-13 14:41:27 +0200

    FindCommonItems playground of Algorithms course

---
+++ Algorithms/Master the Coding Interview/FindCommonItems.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+func findCommonItems(firstArray: [String], secondArray: [String]) -> [String {
+    var set = Set<String>()
+    firstArray.forEach { item in
+        set.insert(item)
+    }
+    
+    var result = [String]()
+    
+    secondArray.forEach { item in
+        if set.contains(item) {
+            result.append(item)
+        }
+    }
+    return result
+}
+
+let array1 = ["a","b","c","x"]
+let array2 = ["d","f","x","a"]
+
+findCommonItems(firstArray: array1, secondArray: array2)
