f245b7050966edff68121f3befec120228e67ba3
Roman Bohun <roman.v.bogun@hotmail.com>
2020-10-27 11:49:24 +0200

    added another approach of how to do not re-fetch data on screen

--- Flutter/my_shop/lib/orders/orders_screen.dart
+++ Flutter/my_shop/lib/orders/orders_screen.dart
@@ -1,49 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:my_shop/common/classes/result.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/orders_provider.dart';
 import '../common/drawer/app_drawer.dart';
 import '../orders/order_item.dart';
 
-class OrdersScreen extends StatefulWidget {
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isDataLoadingInProgress = false;
-
-  @override
-  void initState() {
-    _isDataLoadingInProgress = true;
-    Provider.of<OrdersProvider>(context, listen: false)
-        .fetchAndSet()
-        .then((_) {
-      setState(() {
-        _isDataLoadingInProgress = false;
-      });
-    });
-
-    super.initState();
-  }
-
+// This is used in case if it just shows a data
+class OrdersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final ordersProvider = Provider.of<OrdersProvider>(context);
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Your Orders'),
-      ),
-      drawer: AppDrawer(),
-      body: _isDataLoadingInProgress
-          ? Center(
-        child: CircularProgressIndicator(),
-      )
-          : ListView.builder(
-        itemCount: ordersProvider.orders.length,
-        itemBuilder: (ctx, index) => OrderItem(ordersProvider.orders[index]),
-      ),
+        appBar: AppBar(
+          title: Text('Your Orders'),
+        ),
+        drawer: AppDrawer(),
+        body: FutureBuilder(
+          future: Provider.of<OrdersProvider>(context, listen: false)
+              .fetchAndSet(),
+          builder: (ctx, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              final result = dataSnapshot.data as Result;
+              if (result != null && !result.success) {
+                return Center(
+                  child: Text(result.failure.message),
+                );
+              } else {
+                return Consumer<OrdersProvider>(
+                    builder: (ctx, ordersProvider, _) => ListView.builder(
+                      itemCount: ordersProvider.orders.length,
+                      itemBuilder: (ctx, index) => OrderItem(ordersProvider.orders[index]),
+                    )
+                );
+              }
+            }
+          },
+        )
     );
   }
 }
+
+// // This approach is used when we call others screens from here and then get back here
+// // and we don't want to re-fetch data
+// class OrdersScreen extends StatefulWidget {
+//   @override
+//   _OrdersScreenState createState() => _OrdersScreenState();
+// }
+//
+// class _OrdersScreenState extends State<OrdersScreen> {
+//   Future _ordersFuture;
+//
+//   Future _obtainOrdersFuture() {
+//     return Provider.of<OrdersProvider>(context, listen: false)
+//         .fetchAndSet();
+//   }
+//
+//   @override
+//   void initState() {
+//     _ordersFuture = _obtainOrdersFuture();
+//     super.initState();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     // final ordersProvider = Provider.of<OrdersProvider>(context);
+//
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: Text('Your Orders'),
+//         ),
+//         drawer: AppDrawer(),
+//         body: FutureBuilder(
+//           future: _ordersFuture,
+//           builder: (ctx, dataSnapshot) {
+//             if (dataSnapshot.connectionState == ConnectionState.waiting) {
+//               return Center(
+//                 child: CircularProgressIndicator(),
+//               );
+//             } else {
+//               final result = dataSnapshot.data as Result;
+//               if (result != null && !result.success) {
+//                 return Center(
+//                   child: Text(result.failure.message),
+//                 );
+//               } else {
+//                 return Consumer<OrdersProvider>(
+//                     builder: (ctx, ordersProvider, _) => ListView.builder(
+//                       itemCount: ordersProvider.orders.length,
+//                       itemBuilder: (ctx, index) => OrderItem(ordersProvider.orders[index]),
+//                     )
+//                 );
+//               }
+//             }
+//           },
+//         )
+//     );
+//   }
+// }
