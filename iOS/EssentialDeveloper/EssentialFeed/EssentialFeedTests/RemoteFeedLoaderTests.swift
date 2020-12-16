f244cd10fcaa15e504eb9a0d2cfde98d8b80623a
Roman Bohun <roman.v.bogun@hotmail.com>
2020-12-16 16:36:31 +0200

    Added RemoteFeedLoaderTests

---
+++ iOS/EssentialDeveloper/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,59 @@
+//
+//  EssentialFeedTests.swift
+//  EssentialFeedTests
+//
+//  Created by Roman Bogun on 16.12.2020.
+//
+
+import XCTest
+@testable import EssentialFeed
+
+class RemoteFeedLoader {
+    let client: HTTPClient
+    let url: URL
+
+    init (url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+
+    func load() {
+        client.get(from: url)
+    }
+}
+
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let (_, client) = makeSut()
+
+        XCTAssertNil(client.requestedURL)
+    }
+
+    func test_load_requestdataFromURL() {
+        let url = URL(string: "https://a-given-url.com")
+        let (sut, client) = makeSut(url: url!)
+
+        sut.load()
+
+        XCTAssertEqual(client.requestedURL, url)
+    }
+
+    private func makeSut(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+
+        func get(from url: URL) {
+            requestedURL = url
+        }
+    }
+}
