3068e087ce076979436e6457011b6684d7c5ace2
Roman Bohun <roman.v.bogun@hotmail.com>
2020-12-19 11:51:32 +0200

    Deliver invalidData error on non 200 HTTP response

--- iOS/EssentialDeveloper/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ iOS/EssentialDeveloper/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -49,6 +49,19 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(capturedError, [.connectivity])
     }
 
+    func test_load_deliversErrorOnNon200HTTPReponse() {
+        let (sut, client) = makeSut()
+
+        var capturedError = [RemoteFeedLoader.Error]()
+        sut.load { error in
+            capturedError.append(error)
+        }
+
+        client.complete(with: 400)
+
+        XCTAssertEqual(capturedError, [.invalidData])
+    }
+
     // MARK: - Helpers
 
     private func makeSut(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -58,17 +71,29 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
 
     private class HTTPClientSpy: HTTPClient {
-        private var messages = [(url: URL, completion: (Error) -> Void)]()
+        private var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
 
         var requestedURLs: [URL] {
             return messages.map { $0.url }
         }
-        func get(from url: URL, completion: @escaping (Error) -> Void) {
+        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
             messages.append((url, completion))
         }
 
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error)
+            messages[index].completion(error, nil)
         }
+
+        func complete(with statusCode: Int, at index: Int = 0 ) {
+            let response = HTTPURLResponse(
+                url: requestedURLs[index],
+                statusCode: statusCode,
+                httpVersion: nil,
+                headerFields: nil
+            )
+
+            messages[index].completion(nil, response)
+        }
+
     }
 }
